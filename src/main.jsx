fb0ebac471d1c60c3550de6d33b9ab1f588c7e38
winniesss <75696233+winniesss@users.noreply.github.com>
2026-04-22 11:55:21 -0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import DeskCalendar from './DeskCalendar.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <DeskCalendar />
+        </React.StrictMode>,
+        )
